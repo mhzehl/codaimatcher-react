7cefda9489600941f3b76d121d2e2cfe1486ddd1
Melvin Zehl <mhzehl@gmail.com>
2017-02-25 17:38:44 +0100

    Add Material UI

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import Navigation from './components/Navigation'
 import Title from './components/Title'
 import './App.sass'
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import muiTheme from './assets/styles/theme'
+
 class App extends React.Component {
+  static childContextTypes = {
+    muiTheme: React.PropTypes.object.isRequired,
+  }
+
+  getChildContext() {
+    return { muiTheme }
+  }
+  
   render() {
     return (
       <div className='app'>
