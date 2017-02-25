d986086f9c5bcd3e723d7058d9f20e26fe11b035
Melvin Zehl <mhzehl@gmail.com>
2017-02-25 16:20:07 +0100

    Create title component and add to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Title from './components/Title'
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <Title />
     )
   }
 }
