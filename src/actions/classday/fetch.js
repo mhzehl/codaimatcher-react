845f71225ec46d29948412bb74d77eb1326efb30
Melvin Zehl <mhzehl@gmail.com>
2017-02-27 07:13:07 +0100

    Fix fetch

--- src/actions/classday/fetch.js
+++ src/actions/classday/fetch.js
@@ -8,44 +8,49 @@ export const FETCHED_CLASSDAY = 'FETCHED_CLASSDAY'
 const api = new API()
 const classdays = api.service('classdays')
 
-export default (currentUser) => {
+export default () => {
   return (dispatch) => {
     dispatch(loading(true))
 
-    classdays.find({
-      query: {
-        $limit: 1,
-        createdAt: { '$gte': new Date().setHours(0, 0, 0) }
-      }
-    })
+    api.app.authenticate()
     .then((response) => {
-      if (response.data.length) {
-        dispatch(loadSuccess())
-        dispatch({
-          type: FETCHED_CLASSDAY,
-          payload: response.data
-        })
-      }
-      else {
-        classdays.create({
-          userId: currentUser._id
-        })
-        .then((response) => {
+      classdays.find({
+        query: {
+          $limit: 1,
+          createdAt: { '$gte': new Date().setHours(0, 0, 0) }
+        }
+      })
+      .then((response) => {
+        if (response.data.length) {
+          dispatch(loadSuccess())
           dispatch({
             type: FETCHED_CLASSDAY,
-            payload: response
+            payload: response.data
           })
-        })
-        .catch((error) => {
-          dispatch(loadError(error))
-        })
-      }
-    })
-    .catch((error) => {
-      dispatch(loadError(error))
-    })
-    .then(() => {
-      dispatch(loading(false))
+          // ToDo:
+          // Find my pair in response.data[0].pairs
+          // Fetch user where _id is in my pair
+          // Store in state by dispatching an action
+        }
+        else {
+          classdays.create({})
+          .then((response) => {
+            dispatch({
+              type: FETCHED_CLASSDAY,
+              payload: response
+            })
+          })
+          .catch((error) => {
+            dispatch(loadError(error))
+          })
+        }
+      })
+      .catch((error) => {
+        dispatch(loadError(error))
+      })
+      .then(() => {
+        dispatch(loading(false))
+      })
     })
   }
 }
